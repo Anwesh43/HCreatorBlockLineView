8487bb1c3d0c8e6771e38cf917802d9200fe64c4
Anwesh43 <anweshthecool0@gmail.com>
2020-07-24 02:47:13 +0530

    adding renderer in view

--- hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
+++ hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
@@ -75,14 +75,16 @@ fun Canvas.drawHCBNode(i : Int, scale : Float, paint : Paint) {
 
 class HCreatorBlockView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -205,8 +207,9 @@ class HCreatorBlockView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val hcb : HCreatorBlock = HCreatorBlock(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             hcb.draw(canvas, paint)
             animator.animate {

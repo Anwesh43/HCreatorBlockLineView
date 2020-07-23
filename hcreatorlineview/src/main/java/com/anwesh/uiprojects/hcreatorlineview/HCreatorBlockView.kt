a6b2e1521d08e8c69dda160461456393f2f5dcba
Anwesh43 <anweshthecool0@gmail.com>
2020-07-24 02:32:00 +0530

    adding HCBNode class

--- hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
+++ hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
@@ -136,4 +136,45 @@ class HCreatorBlockView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class HCBNode(var i : Int, val state : State = State()) {
+
+        private var next : HCBNode? = null
+        private var prev : HCBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HCBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHCBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HCBNode {
+            var curr : HCBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
 }

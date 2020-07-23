963b64c636a2f51c24d4233f0e17e2ea3468c852
Anwesh43 <anweshthecool0@gmail.com>
2020-07-24 02:42:40 +0530

    adding HCreatorBlock class

--- hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
+++ hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
@@ -177,4 +177,27 @@ class HCreatorBlockView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class HCreatorBlock(var i : Int) {
+
+        private var curr : HCBNode = HCBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
 }

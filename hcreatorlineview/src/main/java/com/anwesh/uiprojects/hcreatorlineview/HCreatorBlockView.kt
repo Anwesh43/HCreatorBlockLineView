31240087f741f86cbad6a55d4802292dc10d13db
Anwesh43 <anweshthecool0@gmail.com>
2020-07-24 02:04:12 +0530

    adding constants and libraries

--- hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
+++ hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.hcreatorlineview
 /**
  * Created by anweshmishra on 24/07/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+
+val colors : Array<String> = arrayOf("#F44336", "#4CAF50", "#3F51B5", "#FF5722", "#03A9F4")
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 6f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+

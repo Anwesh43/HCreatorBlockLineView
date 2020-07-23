54e601cc45d77ac936f556219f435f55e3567ace
Anwesh43 <anweshthecool0@gmail.com>
2020-07-24 02:23:24 +0530

    adding HCreatorBlockView

--- hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
+++ hcreatorlineview/src/main/java/com/anwesh/uiprojects/hcreatorlineview/HCreatorBlockView.kt
@@ -72,3 +72,20 @@ fun Canvas.drawHCBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHCreatorBlocks(scale, w, h, paint)
 }
+
+class HCreatorBlockView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}

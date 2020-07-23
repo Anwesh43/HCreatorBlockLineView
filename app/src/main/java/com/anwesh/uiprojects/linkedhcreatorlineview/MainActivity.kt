3dbcfc24dd74f2e3d69300abe348722a767d87e9
Anwesh43 <anweshthecool0@gmail.com>
2020-07-24 02:55:54 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhcreatorlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhcreatorlineview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedhcreatorlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.hcreatorlineview.HCreatorBlockView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HCreatorBlockView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
